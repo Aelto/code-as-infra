6976dc46b1dc46c7d1c4aa911b56c776fd5d3ae1
Aelto <Aelto@users.noreply.github.com>
2025-12-02 18:05:32 +0100

    reverse-proxy: move tls function to tls module

--- infrastructure/reverse-proxy/src/proxy/mod.rs
+++ infrastructure/reverse-proxy/src/proxy/mod.rs
@@ -1,49 +1,13 @@
-use pingora::listeners::tls::TlsSettings;
-use pingora::proxy::http_proxy_service;
-use pingora::server::configuration::ServerConf;
-use std::sync::Arc;
+mod app;
+use app::ProxyApp;
 
 mod tls;
 pub use tls::HostConfigTls;
 pub use tls::TlsHandler;
-
-mod app;
-use app::ProxyApp;
-
-pub mod context;
+pub use tls::proxy_service_tls;
 
 mod plain;
 pub use plain::HostConfigPlain;
 pub use plain::proxy_service_plain;
 
-use crate::proxy::context::WithProxyContext;
-
-// TODO: move it to the tls module
-pub fn proxy_service_tls<'server, 'service, CONTEXT>(
-    server_conf: &'server Arc<ServerConf>,
-    listen_addr: &str,
-    host_configs: Vec<HostConfigTls>,
-) -> impl pingora::services::Service + use<'service, CONTEXT>
-where
-    'service: 'server,
-    CONTEXT: WithProxyContext + 'static,
-{
-    let plain_host_config = host_configs
-        .iter()
-        .map(|x| HostConfigPlain {
-            proxy_internal_address: x.proxy_internal_address.clone(),
-            proxy_internal_tls: x.proxy_internal_tls,
-            proxy_hostname: x.proxy_hostname.clone(),
-        })
-        .collect();
-
-    let proxy_app = ProxyApp::<CONTEXT>::new(plain_host_config);
-    let mut service = http_proxy_service(server_conf, proxy_app);
-
-    let cb = TlsHandler::new(host_configs);
-    let cb = Box::new(cb);
-    let tls_settings = TlsSettings::with_callbacks(cb).unwrap();
-    service.add_tls_with_settings(listen_addr, None, tls_settings);
-
-    service
-}
+pub mod context;
--- infrastructure/reverse-proxy/src/proxy/tls.rs
+++ infrastructure/reverse-proxy/src/proxy/tls.rs
@@ -1,7 +1,13 @@
+use std::sync::Arc;
+
 use pingora::{
-    listeners::TlsAccept,
+    listeners::{TlsAccept, tls::TlsSettings},
+    proxy::http_proxy_service,
+    server::configuration::ServerConf,
     tls::{self, ssl},
 };
+
+use crate::proxy::{HostConfigPlain, app::ProxyApp, context::WithProxyContext};
 
 #[derive(Clone)]
 pub struct HostConfigTls {
@@ -77,4 +83,33 @@ impl TlsAccept for TlsHandler {
             }
         }
     }
+}
+
+pub fn proxy_service_tls<'server, 'service, CONTEXT>(
+    server_conf: &'server Arc<ServerConf>,
+    listen_addr: &str,
+    host_configs: Vec<HostConfigTls>,
+) -> impl pingora::services::Service + use<'service, CONTEXT>
+where
+    'service: 'server,
+    CONTEXT: WithProxyContext + 'static,
+{
+    let plain_host_config = host_configs
+        .iter()
+        .map(|x| HostConfigPlain {
+            proxy_internal_address: x.proxy_internal_address.clone(),
+            proxy_internal_tls: x.proxy_internal_tls,
+            proxy_hostname: x.proxy_hostname.clone(),
+        })
+        .collect();
+
+    let proxy_app = ProxyApp::<CONTEXT>::new(plain_host_config);
+    let mut service = http_proxy_service(server_conf, proxy_app);
+
+    let cb = TlsHandler::new(host_configs);
+    let cb = Box::new(cb);
+    let tls_settings = TlsSettings::with_callbacks(cb).unwrap();
+    service.add_tls_with_settings(listen_addr, None, tls_settings);
+
+    service
 }
